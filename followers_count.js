e2c899deef040feb9c17be72265193ea395f6957
tangkwok0104 <kwoktang600@gmail.com>
2024-11-07 13:50:50 -0500

    update js and html

---
+++ followers_count.js
@@ -0,0 +1,21 @@
+let count = 0; // Initialize count to 0
+
+function increaseCount() {
+    count++; // Increment the count by 1
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and display messages
+}
+
+//check the value of count and trigger alerts when specific follower milestones
+function checkCountValue() {
+    if (count === 10) {
+      alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20) {
+      alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
+
+  function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
+}
+
